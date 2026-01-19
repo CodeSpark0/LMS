ee176d253c3089c7aa36c85928044e04c055c061
alik.kuttybayev <alik.kuttybayev@gmail.com>
2026-01-19 11:43:48 +0500

    update

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class Main {
+public class    Main {
     public static void main(String[] args) {
         String url = " ";
         String name = " ";
